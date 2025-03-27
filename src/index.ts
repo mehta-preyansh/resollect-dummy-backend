94cb9387719b43aff3cd5f4a1fe4e276b5e705c6
mehta-preyansh <preyanshmehtaben@gmail.com>
2025-03-27 18:54:59 +0530

    [Fixed] Cors error

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors({
+  origin: "*", 
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+}))
+
 app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
